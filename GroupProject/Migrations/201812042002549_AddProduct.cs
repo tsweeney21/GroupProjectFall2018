f41e234be6630d270bcda952234536296a7af670
BPaxton <bp278031@southeast.edu>
2018-12-04 14:14:06 -0600

    product

---
+++ GroupProject/Migrations/201812042002549_AddProduct.cs
@@ -0,0 +1,16 @@
+namespace GroupProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProduct : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
