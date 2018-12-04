4dc2927d86cacb6c85a0296570d5db0a6ce2b403
BPaxton <bp278031@southeast.edu>
2018-12-04 13:46:47 -0600

    updated product model

--- GroupProject/Models/Product.cs
+++ GroupProject/Models/Product.cs
@@ -1,34 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace GroupProject.Models
 {
+    [Table("Products")]
     public class Product
     {
 
-        //test rollback
+        
 
+        [Key]
         public int prodID { get; set; }
+        [Required]
+        [MaxLength(255)]
+        [Display(Name = "Product Name")]
         public string prodName { get; set; }
+        [Required]
+        [MaxLength(255)]
+        [Display(Name = "Product Description")]
         public string prodDescription { get; set; }
+        [Required]
+        [Range(0, 99.99)]
+        [Display(Name = "Product Price")]
         public decimal prodPrice { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string prodPicture { get; set; }
 
-        public Product()
-        {
-
-        }
-
-        public Product(int prodID, string prodName, string prodDescription, decimal prodPrice, string prodPicture)
-        {
-            this.prodID = prodID;
-            this.prodName = prodName;
-            this.prodDescription = prodDescription;
-            this.prodPrice = prodPrice;
-            this.prodPicture = prodPicture;
-                
-        }
     }
 }
