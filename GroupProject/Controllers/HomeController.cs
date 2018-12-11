5b660762cc083efdb333c8d0f1e25468fa92615c
bw447997 <bw447997@LN01004>
2018-12-11 14:01:17 -0600

    puuuuush it

--- GroupProject/Controllers/HomeController.cs
+++ GroupProject/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace GroupProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Rawks are rocks.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Rock contact page.";
 
             return View();
         }
